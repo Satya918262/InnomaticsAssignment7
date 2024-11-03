6f62968df2aad4ddf0c648dae013c73dd6f096c9
Satya918262 <satyavathimirthipati@gmail.com>
2024-11-03 20:07:03 +0530

    Updates

---
+++ precache-manifest.f49e020f644693b049587cc7e20a15f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400e4c00ffb935da2c31f6c605326afc",
+    "url": "/Innomaticsassignment7/index.html"
+  },
+  {
+    "revision": "805f090a88117bebc238",
+    "url": "/Innomaticsassignment7/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "987cf3052742e0c4dd30",
+    "url": "/Innomaticsassignment7/static/css/main.aa557453.chunk.css"
+  },
+  {
+    "revision": "805f090a88117bebc238",
+    "url": "/Innomaticsassignment7/static/js/2.2e986b30.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/Innomaticsassignment7/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "987cf3052742e0c4dd30",
+    "url": "/Innomaticsassignment7/static/js/main.263bb67e.chunk.js"
+  },
+  {
+    "revision": "ba0b2cf28524932ac46d",
+    "url": "/Innomaticsassignment7/static/js/runtime~main.15033371.js"
+  }
+]);
